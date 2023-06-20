ce62d18fa07dbacd60dfd588e5ce88d5b5e2838f
John Aughey <jha@aughey.com>
2023-06-20 23:49:22 +0000

    initial version that seems to work

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rp2040_loader
+
+Create a web server and writes the posted file to an attached rp2040 pico.
+
+```
+rustup target add x86_64-pc-windows-gnu
+sudo apt update
+sudo apt-get install mingw-w64
+```
+
+```
+curl -X POST -F "firmware=@/etc/passwd" http://host.docker.internal:3000/upload
+```
---
+++ src/main.rs
@@ -0,0 +1,147 @@
+use anyhow::Result;
+use axum::{extract::Multipart, extract::State, response::IntoResponse, routing::post, Router};
+use axum_macros::debug_handler;
+use clap::Parser;
+use std::sync::{Arc, Mutex};
+
+type AppState = Arc<Mutex<dyn FnMut(Vec<u8>) + Send>>;
+
+// clap derive args
+#[derive(Parser)]
+struct Args {
+    /// File to look for for changes that we will copy to the output
+    #[clap(long)]
+    watch_file: String,
+}
+
+struct FirmwareHandler {
+    pending_firmware: Option<Vec<u8>>,
+    firmware_path: String,
+}
+impl FirmwareHandler {
+    fn new(firmware_path: String) -> Self {
+        Self {
+            pending_firmware: None,
+            firmware_path,
+        }
+    }
+    fn new_firmware(&mut self, data: Vec<u8>) {
+        self.pending_firmware = Some(data);
+        self.try_write_to_2040();
+    }
+    fn try_write_to_2040(&mut self) {
+        println!("Trying to write firmware");
+        match self.pending_firmware.as_ref() {
+            None => {
+                println!("No pending firmware");
+                return;
+            }
+            Some(firmware) => {
+                println!("Pending firmware exists");
+                if std::path::Path::new(&self.firmware_path).exists() {
+                    println!("Firmware location exists");
+                    println!("Writing Firmware...");
+                    // write firmware to file
+
+                    match std::fs::write(self.firmware_path.clone() + "/firmware", firmware) {
+                        Ok(_) => {
+                            println!("Firmware written");
+                            self.pending_firmware = None;
+                        }
+                        Err(e) => println!("Error writing firmware: {}", e),
+                    }
+                } else {
+                    println!("Firmware path doesn't exist: {}", self.firmware_path);
+                }
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let handler = Arc::new(Mutex::new(FirmwareHandler::new(args.watch_file.clone())));
+
+    #[allow(unreachable_code)]
+    let monitor_plugin = {
+        let handler = handler.clone();
+        async move {
+            loop {
+                // pico not plugged in
+                let searchpath = std::path::Path::new(&args.watch_file);
+                loop {
+                    if searchpath.exists() {
+                        println!("Pico found");
+                        break;
+                    }
+                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                }
+
+                // pico plugged in
+                handler.lock().unwrap().try_write_to_2040();
+
+                // wait for pico to be unplugged
+                loop {
+                    if !searchpath.exists() {
+                        println!("Pico unplugged");
+                        break;
+                    }
+                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                }
+            }
+            Ok(())
+        }
+    };
+
+    let on_new_firmware: AppState = {
+        let handler = handler.clone();
+        Arc::new(Mutex::new(move |data| {
+            let mut handler = handler.lock().unwrap();
+            handler.new_firmware(data);
+        }))
+    };
+
+    let app = Router::new()
+        .route("/upload", post(upload_handler))
+        .with_state(on_new_firmware);
+
+    println!("listening on http://localhost:3000");
+    let server =
+        axum::Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(app.into_make_service());
+
+    // join all tasks
+    tokio::try_join!(server, monitor_plugin)?;
+
+    Ok(())
+}
+
+#[debug_handler]
+async fn upload_handler(
+    State(state): State<AppState>,
+    mut multipart: Multipart,
+) -> Result<impl IntoResponse, String> {
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .map_err(|e| format!("Could not get next field: {}", e))?
+    {
+        let name = field.name().unwrap();
+        // only accept firmware content
+        if name != "firmware" {
+            continue;
+        }
+        let data = field
+            .bytes()
+            .await
+            .map_err(|e| format!("Could not get field bytes: {}", e))?;
+
+        let mut on_new_data = state.lock().unwrap();
+
+        on_new_data(data.to_vec());
+
+        //        println!("Length of `{}` is {} bytes", name, data.len());
+    }
+    Ok(())
+}
